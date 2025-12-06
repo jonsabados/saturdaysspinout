dd5bf5f579b55c243bf6f218404c2ccea89531e8
Jon Sabados <jon.sabados@gmail.com>
2025-12-05 18:03:24 -0700

    Front end scaffolding

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Hello World</h1>
+    <p>Saturday's Racelog</p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+h1 {
+  font-size: 3rem;
+  margin-bottom: 0.5rem;
+}
+
+p {
+  color: #666;
+}
+</style>
